4cb60095297636ae0f82cff2c630109a501d56ae
cjain2710 <cjain2710@gmail.com>
2026-06-20 01:02:21 +1000

    Update screener.py

--- data/screener.py
+++ data/screener.py
@@ -1,18 +1,5 @@
 from config.universe import UNIVERSE
-from data.market_data import get_daily
-from strategy.filters import trend_filter
 
 def screen_stocks():
-    selected = []
-
-    for stock in UNIVERSE:
-        try:
-            df = get_daily(stock)
-
-            if trend_filter(df):
-                selected.append(stock)
-
-        except:
-            continue
-
-    return selected
+    # TEMP SAFE MODE (always return stocks)
+    return UNIVERSE
