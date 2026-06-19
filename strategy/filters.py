5b8fa91fd1f68f6f788b96f92dcc9b7fc7c52b5a
cjain2710 <cjain2710@gmail.com>
2026-06-20 01:08:20 +1000

    Update filters.py

--- strategy/filters.py
+++ strategy/filters.py
@@ -1,12 +1,2 @@
-import pandas_ta as ta
-
-def trend_filter(daily_df):
-    if len(daily_df) < 200:
-        return False
-
-    daily_df["MA50"] = ta.sma(daily_df["Close"], length=50)
-    daily_df["MA200"] = ta.sma(daily_df["Close"], length=200)
-
-    latest = daily_df.iloc[-1]
-
-    return latest["Close"] > latest["MA50"] > latest["MA200"]
+def is_active_stock(df):
+    return (df["High"].max() - df["Low"].min()) > 0.5
